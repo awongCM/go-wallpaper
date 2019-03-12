4adc5b31f145a75467e16d7974bcfd0848594094
awongCM <awong.cm@gmail.com>
2019-03-12 23:07:38 +1100

    done one for mac. woohoo!

--- go-wallpaper.go
+++ go-wallpaper.go
@@ -4,12 +4,13 @@ import (
 	"fmt"
 	"os"
 	"os/exec"
+	"os/signal"
 	"path/filepath"
 	"runtime"
 	"strings"
+	"syscall"
+	"time"
 )
-
-// TODOS ....
 
 func CheckOSEnviroment() {
 	if runtime.GOOS == "windows" {
@@ -23,6 +24,17 @@ func CheckOSEnviroment() {
 
 func GetCurrentWallpaper() (string, error) {
 	stdout, err := exec.Command("osascript", "-e", `tell application "Finder" to get POSIX path of (get desktop picture as alias)`).Output()
+	if err != nil {
+		return "", err
+	}
+
+	return strings.TrimSpace(string(stdout)), nil
+}
+
+func SetCurrentWallpaper(imageFileLocation string) (string, error) {
+
+	fmt.Println("ready to set wallpaper...")
+	stdout, err := exec.Command("osascript", "-e", `tell application "System Events" to set picture of (reference to current desktop) to "`+imageFileLocation+`"`).Output()
 	if err != nil {
 		return "", err
 	}
@@ -61,9 +73,31 @@ func GetListOfPictures(rootFolder string) {
 		panic(err)
 	}
 
-	for _, file := range files {
-		fmt.Println("file: ", file)
+	// TODO - need to place in another module for this
+	// Keyboard interrupt here
+	c := make(chan os.Signal)
+	signal.Notify(c, os.Interrupt, syscall.SIGTERM)
+	go func() {
+		<-c
+		endItHere()
+		os.Exit(1)
+	}()
+
+	// loop forever until keyboard interrupt signal kicks in
+	for {
+		for _, imageFile := range files {
+			fmt.Println("imageFile: ", imageFile)
+
+			SetCurrentWallpaper(imageFile)
+
+			// test timer to set different wallpaper
+			time.Sleep(20 * time.Millisecond)
+		}
 	}
+}
+
+func endItHere() {
+	fmt.Println("just cancelled...")
 }
 
 func main() {
